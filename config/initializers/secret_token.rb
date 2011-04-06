3d9fa96add459891d47dd7e039335318473cffa2
Moritz Krog <moritzkrog@projectscio.org>
2011-04-06 19:55:22 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Longitude::Application.config.secret_token = '85f1e5f4f2fc8bb7e66028d8baba90e9e4d82f4da99bb2b069671626f2641b92562c35c3641486e417227618b300e0f167b46f850030c59bd3b732bf33e5e560'
