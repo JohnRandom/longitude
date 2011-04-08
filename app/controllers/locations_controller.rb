5f2c50259c6b2b20f9d6ce45238b4c37d3ae1336
Moritz Krog <moritzkrog@projectscio.org>
2011-04-08 14:59:38 +0200

    Optimized controller for json output

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -8,9 +8,9 @@ class LocationsController < ApplicationController
 
   def create
     @route = Route.find params[:route_id]
-    @location = @route.locations.create :latitude => params[:location][:latitude], :longitude => params[:location][:longitude]
+    @location = @route.locations.create! :latitude => params[:location][:latitude], :longitude => params[:location][:longitude]
 
     flash.now.alert = "Location '#{@location.latitude}:#{@location.longitude}' on '#{@route.name}' created"
-    redirect_to route_url(@route)
+    redirect_to edit_route_url(@route)
   end
 end
--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,21 +1,22 @@
 class RoutesController < ApplicationController
 
-  # accessors
-  # attribute_accessor :name
-
-  # request validations
+  # before filter
   before_filter :verify_loged_in
   before_filter :verify_params, :only => :create
 
-  # so far, json only
   respond_to :html, :json
 
   def index
     @routes = Route.where :user_id => current_user.id
+
+    respond_to do |format|
+      format.html
+      format.json { render :json => @routes }
+    end
   end
 
   def show
-    @route = Route.find_by_id_and_user_id( params[:id], current_user.id )
+    @route = Route.find_by_id_and_user_id!( params[:id], current_user.id )
 
     respond_to do |format|
       format.html
@@ -28,13 +29,14 @@ class RoutesController < ApplicationController
   end
 
   def create
+    p params
     route_name = params[:name]
 
     @route = Route.create! :name => route_name, :user_id => current_user.id
     redirect_to :action => :index and return
 
     respond_to do |format|
-      format.json  { render :json => @route}
+      format.json  { render :json => @route }
     end
   end
 
@@ -43,17 +45,24 @@ class RoutesController < ApplicationController
   end
 
   def update
-    @route = Route.find_by_id_and_user_id params[:id], current_user.id
+    @route = Route.find_by_id_and_user_id! params[:id], current_user.id
     @route.update_attribute( :name, params[:route][:name] ) if @route
-    redirect_to :action => :index
+
+      respond_to do |format|
+      format.html { redirect_to :action => :index and return }
+      format.json { render :json => @route }
+    end
   end
 
   def destroy
-    @route = Route.find_by_id_and_user_id params[:id], current_user.id
+    @route = Route.find_by_id_and_user_id! params[:id], current_user.id
     @route.destroy if @route
 
     flash.now.alert = "Route '#{@route.name}' deleted."
-    redirect_to :action => :index
+    respond_to do |format|
+      format.html { redirect_to :action => :index and return }
+      format.json { head :deleted }
+    end
   end
 
   private
