0599b274269bb4ce5311cf250c41291f6739ae1e
Moritz Krog <moritzkrog@projectscio.org>
2011-04-08 16:46:25 +0200

    Playing with rspec testing

---
+++ spec/controllers/routes_controllers_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe RoutesController do
+
+  before(:each) do
+      @user = log_in
+    end
+
+  describe 'show' do
+    it 'should fetch the right route' do
+      Route.should_receive(:find_by_id_and_user_id!).with(3, @user.id).and_return(stub.as_null_object)
+      get :show, :id => 3
+    end
+  end
+
+  describe 'create' do
+    it 'should create the right record' do
+      Route.should_receive(:create!).with(:name => :test_route, :user_id => @user.id).and_return(stub.as_null_object)
+      post :create, :route => {:name => :test_route}
+    end
+
+    it 'should raise an error on invalid post data' do
+      Route.create!
+    end
+  end
+end
