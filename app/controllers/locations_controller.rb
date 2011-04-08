dd7cb568ed5c94cd17ebcd47b0643bfdd73c6e85
Moritz Krog <moritzkrog@projectscio.org>
2011-04-08 15:31:13 +0200

    Added a delete location button to location table

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -11,19 +11,21 @@ class LocationsController < ApplicationController
     long = params[:location][:longitude]
 
     @route = Route.find params[:route_id]
-    @location = @route.locations.create! :latitude => lat, :longitude => long :user_id => current_user.id
+    @location = @route.locations.create! :latitude => lat, :longitude => long, :user_id => current_user.id
 
     flash.now.alert = "Location '#{@location.latitude}:#{@location.longitude}' on '#{@route.name}' created"
     redirect_to edit_route_url(@route)
   end
 
   def destroy
-    @route = Route.find_by_id_and_user_id params[:id], current_user.id
-    @route.destroy if @route
+    @location = Location.find_by_id_and_user_id params[:id], current_user.id
+    @location.destroy if @location
+
+    logger.debug "edit_route_url: #{edit_route_url params[:route_id]}"
 
     flash.now.alert = "Location deleted."
     respond_to do |format|
-      format.html { redirect_to :controller => :routes, :action => :index and return }
+      format.html { redirect_to edit_route_url(params[:route_id]) and return }
       format.json { head :deleted }
     end
   end
